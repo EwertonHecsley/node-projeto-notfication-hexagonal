b76a5f181518aae29988f3a6e8e8bb9adfea82ae
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-06-18 12:13:54 -0300

    feat: construcao do dominio, entidade Notification.

---
+++ src/domain/entity/Notification.ts
@@ -0,0 +1,30 @@
+export class Notification {
+    private constructor(
+        private readonly _to: string,
+        private readonly _message: string,
+        private readonly _type: "email" | "sms"
+    ) {
+        this._message = _message;
+        this._to = _to;
+        this._type = _type;
+    }
+
+    static create(to: string, message: string, type: "email" | "sms"): Notification {
+        if (!to || !message) throw new Error("Recipient and message are required.");
+        if (!["email", "sms"].includes(type)) throw new Error("Invalid notification type.");
+
+        return new Notification(to, message, type);
+    }
+
+    get to(): string {
+        return this._to;
+    }
+
+    get message(): string {
+        return this._message;
+    }
+
+    get type(): string {
+        return this._type;
+    }
+}
